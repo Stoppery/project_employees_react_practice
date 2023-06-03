5a8d5290cf100fa45f8aeeaf365185f4150df361
Maxim <max.miromanov@yandex.ru>
2023-06-03 22:32:24 +0300

    initial commit

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,21 @@
+import './app-info.css';
+
+
+const AppInfo = ({increased, employees}) => {
+		return (
+			<div className="app-info">
+				<h1>
+					Учет сотрудников в компании Stoppery
+				</h1>
+				<h2>
+					Общее число сотрудников: {employees}
+				</h2>
+				<h2>
+					Премию получат: {increased}
+				</h2>
+			</div>
+		)
+}
+
+
+export default AppInfo;
